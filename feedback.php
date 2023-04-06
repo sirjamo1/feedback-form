685b55dfeefdd64dfa198dc72a18a4f7f6ec3deb
Sirjamo1 <jam_how@hotmail.com>
2023-04-06 14:29:51 +1000

    form validation

--- feedback.php
+++ feedback.php
@@ -1,8 +1,49 @@
 <?php include 'inc/header.php'; ?>
 
-<h2>Feedback</h2>
+<?php
+$sql = 'SELECT * FROM feedback';
+$result = mysqli_query($conn, $sql);
+$feedback = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
-<div class="card my-3">
+// $feedback = [
+//   [
+//     'id' => '1',
+//     'name' => 'bill',
+//     'email' => 'bill@hotmail.com',
+//     'body' => 'i am bill'
+//   ],
+//   [
+//     'id' => '2',
+//     'name' => 'will',
+//     'email' => 'will@hotmail.com',
+//     'body' => 'i am will'
+//   ],
+//   [
+//     'id' => '3',
+//     'name' => 'shill',
+//     'email' => 'shill@hotmail.com',
+//     'body' => 'i am shill'
+//   ]
+// ]
+
+?>
+<h2>Past Feedback</h2>
+<?php if (empty($feedback)) : ?>
+  <p class="lead mt3">There is no feedback...</p>
+<?php endif; ?>
+<?php foreach ($feedback as $item) : ?>
+  <div class="card my-3 w-75">
+    <div class="card-body text-center">
+      <?php echo $item['body']; ?>
+      <div class="text-secondary mt-2">
+        By <?php echo $item['name']; ?>
+        <br>
+        Posted on <?php echo $item['date'] ?>
+      </div>
+    </div>
+  </div>
+<?php endforeach; ?>
+<!-- <div class="card my-3">
   <div class="card-body">
     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta molestias animi earum eos dolorem repellat a quibusdam, aperiam vero repellendus voluptatibus natus deserunt sed doloribus inventore, totam labore maxime perferendis!
   </div>
@@ -12,11 +53,5 @@
   <div class="card-body">
     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta molestias animi earum eos dolorem repellat a quibusdam, aperiam vero repellendus voluptatibus natus deserunt sed doloribus inventore, totam labore maxime perferendis!
   </div>
-</div>
-
-<div class="card my-3">
-  <div class="card-body">
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta molestias animi earum eos dolorem repellat a quibusdam, aperiam vero repellendus voluptatibus natus deserunt sed doloribus inventore, totam labore maxime perferendis!
-  </div>
-</div>
+</div> -->
 <?php include 'inc/footer.php'; ?>
--- index.php
+++ index.php
@@ -1,11 +1,38 @@
 <?php include 'inc/header.php'; ?>
+<?php
+$name = $email = $body = '';
+$name_err = $email_err = $body_err = '';
+//form submit
+if (isset($_POST['submit'])) {
+  //validation
+  if (empty($_POST['name'])) {
+    $name_err = 'Name is required';
+  } else {
+    $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_SPECIAL_CHARS);
+  }
+  if (empty($_POST['email'])) {
+    $email_err = 'Email is required';
+  } else {
+    $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+  }
+  if (empty($_POST['body'])) {
+    $body_err = 'Text is required';
+  } else {
+    $body = filter_input(INPUT_POST, 'body', FILTER_SANITIZE_SPECIAL_CHARS);
+  }
+}
+
+?>
 <img src="./img/logo.png" class="w-25 mb-3" alt="">
 <h2>Feedback</h2>
 <p class="lead text-center">Leave feedback for Traversy Media</p>
-<form action="" class="mt-4 w-75">
+<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST" class="mt-4 w-75">
   <div class="mb-3">
     <label for="name" class="form-label">Name</label>
-    <input type="text" class="form-control" id="name" name="name" placeholder="Enter your name">
+    <input type="text" class="form-control <?php echo $name_err ? 'is-invalid' : null ?>" id="name" name="name" placeholder="Enter your name">
+    <div class="invalid-feedback">
+      <?php echo $name_err ?>
+    </div>
   </div>
   <div class="mb-3">
     <label for="email" class="form-label">Email</label>
